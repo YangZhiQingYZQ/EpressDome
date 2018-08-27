e7a56833a8a9d59b6b54092d67e18a3273f9ef80
YangZhiQing <493583130@qq.com>
2018-08-27 14:43:34 +0800

    局部文件渲染

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -7,6 +7,49 @@ app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 //------------
 
+//------------v7.4.6局部文件
+function getWeatherData(){
+	return {
+		locations : [
+			{
+				name : 'Portland',
+				forecastUrl : "http://www.wunderground.com/US/OR/Portland.html",
+				iconUrl : "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
+				weather : 'Overcast',
+				temp:'54.1 F (12.3C)',
+			},
+			{
+				name : "Bend",
+				forecastUrl : "http://www.wunderground.com/US/OR/Bend.html",
+				iconUrl: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
+				weather:'Partly Cloudy',
+				temp : "55.0 F (12.8 C)",
+			},
+			{
+				name : 'Manzanita',
+				forecastUrl : 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl : "http://icons-ak.wxug.com/i/c/k/rain.gif",
+				weather:'Light Rain',
+				temp:"55.0 F (12.8 C)"
+			}
+		]
+	}
+}
+//创建中间件,给res.locals.partials对象添加数据
+app.use((req,res,next)=>{
+	// if(!res.locals.partials){
+	// 	res.locals.partials = {};
+	// }
+	// res.locals.partials.wather = getWeatherData();
+	// next();
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+})
+
+
+//-------------
+
 //----------检测查询字符串的中间件
 app.use((req,res,next)=>{
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === "1";
